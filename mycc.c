c6e0631c65f4c1406c79cb5c057950340e15ccb4
kuisiba <kuisiba@gmail.com>
2020-04-19 19:12:36 +0900

    単項演算子

--- mycc.c
+++ mycc.c
@@ -144,6 +144,7 @@ Node *new_node_num(int val) {
 
 Node *expr(void);
 Node *mul(void);
+Node *unary(void);
 Node *primary(void);
 
 Node *expr(void) {
@@ -160,16 +161,26 @@ Node *expr(void) {
 }
 
 Node *mul(void) {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+Node *unary(void) {
+    if (consume('+')) {
+        return primary();
+    }
+    if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
 }
 
 Node *primary(void) {
